2f0d57aa1f049b56ab7dc0e0b409bce0bf058937
Peter Schramm <you@example.com>
2022-12-29 12:58:41 +0100

    Added experiments

--- 01_generate_staff_detection_dataset.py
+++ 01_generate_staff_detection_dataset.py
@@ -1,12 +1,12 @@
-import json
 import os
 import random
 
-import tqdm
 import albumentations as A
 import cv2
 import numpy as np
-from PIL import Image, ImageDraw
+import tqdm
+
+from utils.dataset import get_sample
 
 DATASET_PATH = os.path.join("..", "..", "datasets", "generated")
 PNG_PATH = os.path.join(DATASET_PATH, "png")
@@ -23,68 +23,6 @@ SPLIT_POINTS = np.cumsum(SPLITS)
 INITIAL_SEED = 41
 NUM_AUG_PER_SAMPLE = 12
 MAX_SAMPLE_IDX = 25
-
-
-def load_mask(pnb_path, sample_idx, mask_idx, hull=False):
-    mask = cv2.imread(
-        f"{pnb_path}/{sample_idx}/out-{mask_idx}.png", cv2.IMREAD_GRAYSCALE
-    )
-    mask[-2:, :] = 0
-    if not hull:
-        return mask
-
-    # create convex hull
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    hulls = []
-    for contour in contours:
-        hull = cv2.convexHull(contour)
-        hulls.append(hull)
-        cv2.drawContours(mask, [hull], 0, (255), -1)
-    # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    return mask
-
-
-def get_sample(sample_idx):
-
-    # load sample from dataset
-    img = cv2.imread(f"{PNG_PATH}/{sample_idx}/out-0.png")
-    img = 255 - cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    # generate staff image from bounding boxes
-    with open(f"{BBOX_PATH}/{sample_idx}.json", "r") as bboxfile:
-        bboxes = json.load(bboxfile)
-    np.unique([bbox["type"] for bbox in bboxes])
-    staffs = [bbox for bbox in bboxes if bbox["type"] == "Staff"]
-    img_h, img_w = img.shape[:2]
-    staff_mask = Image.fromarray(np.zeros_like(img))
-    draw = ImageDraw.Draw(staff_mask)
-    for staff in staffs:
-        x = staff["x"]
-        y = staff["y"]
-        w = staff["width"]
-        h = staff["height"]
-        draw.rectangle(
-            (x * img_w, y * img_h, (x + w) * img_w, (y + h) * img_h), fill="#fff"
-        )
-    staff_mask = cv2.cvtColor(np.array(staff_mask), cv2.COLOR_BGR2GRAY)
-    
-    staff_mask = load_mask(PNG_PATH, sample_idx, 48)
-
-    # generate brace image as convex hulls
-    brace_mask = load_mask(PNG_PATH, sample_idx, 3, hull=True)
-
-    # generate creshendo / diminuendo masks as convex hulls
-    creshendo_mask = load_mask(PNG_PATH, sample_idx, 45, hull=True)
-    diminuendo_mask = load_mask(PNG_PATH, sample_idx, 46, hull=True)
-
-    # combine masks
-    comb_mask = np.zeros_like(img)
-    comb_mask[staff_mask != 0] = (255, 0, 0)
-    comb_mask[brace_mask != 0] = (0, 255, 0)
-    comb_mask[creshendo_mask != 0] = (0, 0, 255)
-    comb_mask[diminuendo_mask != 0] = (0, 255, 255)
-
-    return img, comb_mask
 
 
 # prepare augmentations
@@ -115,7 +53,7 @@ for idx in range(1, MAX_SAMPLE_IDX + 1):
     os.makedirs(out_y_path, exist_ok=True)
     
     sample_idx = f"{idx:03d}"
-    img, comb_mask = get_sample(sample_idx)
+    img, comb_mask = get_sample(PNG_PATH, BBOX_PATH, sample_idx)
     
     print(f"Augmenting sample with idx: {idx}/{MAX_SAMPLE_IDX} to {out_path}", )
     
---
+++ utils/dataset.py
@@ -0,0 +1,67 @@
+import json
+
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw
+
+
+def load_mask(png_path, sample_idx, mask_idx, hull=False):
+    mask = cv2.imread(
+        f"{png_path}/{sample_idx}/out-{mask_idx}.png", cv2.IMREAD_GRAYSCALE
+    )
+    mask[-2:, :] = 0
+    if not hull:
+        return mask
+
+    # create convex hull
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    hulls = []
+    for contour in contours:
+        hull = cv2.convexHull(contour)
+        hulls.append(hull)
+        cv2.drawContours(mask, [hull], 0, (255), -1)
+    # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    return mask
+
+
+def get_sample(png_path, bbox_path, sample_idx):
+
+    # load sample from dataset
+    img = cv2.imread(f"{png_path}/{sample_idx}/out-0.png")
+    img = 255 - cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # generate staff image from bounding boxes
+    with open(f"{bbox_path}/{sample_idx}.json", "r") as bboxfile:
+        bboxes = json.load(bboxfile)
+    np.unique([bbox["type"] for bbox in bboxes])
+    staffs = [bbox for bbox in bboxes if bbox["type"] == "Staff"]
+    img_h, img_w = img.shape[:2]
+    staff_mask = Image.fromarray(np.zeros_like(img))
+    draw = ImageDraw.Draw(staff_mask)
+    for staff in staffs:
+        x = staff["x"]
+        y = staff["y"]
+        w = staff["width"]
+        h = staff["height"]
+        draw.rectangle(
+            (x * img_w, y * img_h, (x + w) * img_w, (y + h) * img_h), fill="#fff"
+        )
+    staff_mask = cv2.cvtColor(np.array(staff_mask), cv2.COLOR_BGR2GRAY)
+    
+    staff_mask = load_mask(png_path, sample_idx, 48)
+
+    # generate brace image as convex hulls
+    brace_mask = load_mask(png_path, sample_idx, 3, hull=True)
+
+    # generate creshendo / diminuendo masks as convex hulls
+    creshendo_mask = load_mask(png_path, sample_idx, 45, hull=True)
+    diminuendo_mask = load_mask(png_path, sample_idx, 46, hull=True)
+
+    # combine masks
+    comb_mask = np.zeros_like(img)
+    comb_mask[staff_mask != 0] = (255, 0, 0)
+    comb_mask[brace_mask != 0] = (0, 255, 0)
+    comb_mask[creshendo_mask != 0] = (0, 0, 255)
+    comb_mask[diminuendo_mask != 0] = (0, 255, 255)
+
+    return img, comb_mask
